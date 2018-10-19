0cc77d1aafd6f0848ffbcb0d0b9aba214c9f99aa
Alastair7 <lekhidugtal@gmail.com>
2018-10-20 01:21:47 +0530

    add new integration

--- command.py
+++ command.py
@@ -1,9 +1,14 @@
 import re
+from news import fetch
+from quote import quote_generator
 
 class Command(object):
     def __init__(self):
         self.commands = {
-            "jump" : self.jump
+            "jump" : self.jump,
+            "help" : self.help,
+            "quote":self.quotes,
+            "news":self.news
         }
  
     def handle_command(self, user, command):
@@ -21,3 +26,16 @@ class Command(object):
     def jump(self, c_list):
         return "Kris Kross will make you jump jump"
      
+    def help(self, c_list):
+        response = "Currently I support the following commands:\r\n"
+         
+        for command in self.commands:
+            response += command + "\r\n"
+
+    def news(self, c_list):
+        return fetch(c_list)
+
+    def quotes(self, c_list):
+        return quote_generator()
+
+       
